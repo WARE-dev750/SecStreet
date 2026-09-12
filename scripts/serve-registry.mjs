0b4591036d95d26233df055b0906c96710873db2
WARE-dev750 <robelsocial750@gmail.com>
2026-09-12 16:18:21 +0000

    chore: server launcher script inside workspace for proper module resolution

---
+++ scripts/serve-registry.mjs
@@ -0,0 +1,9 @@
+import { startRegistryServer } from "@secstreet/service-registry";
+const h = await startRegistryServer({
+  libraryDir: "/workspaces/SecStreet/capabilities/official",
+  port: 8787, host: "127.0.0.1",
+  libraryName: "official", libraryVersion: "0.1.0", maintainer: "SecStreet"
+});
+console.log("registry up at " + h.url);
+process.on("SIGTERM", () => h.close().then(() => process.exit(0)));
+process.on("SIGINT", () => h.close().then(() => process.exit(0)));
